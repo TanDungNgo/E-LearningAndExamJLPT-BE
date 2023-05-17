f7df2c01e94f22efc497218c4c4ccb5ef7f805b8
Tan Dung <tandung12072002@gmail.com>
2023-05-17 15:03:57 +0700

    EL-381 enroll

--- src/main/java/com/example/ElearningAndExamJLPT/service/impl/EnrollmentServiceImpl.java
+++ src/main/java/com/example/ElearningAndExamJLPT/service/impl/EnrollmentServiceImpl.java
@@ -2,15 +2,22 @@ package com.example.ElearningAndExamJLPT.service.impl;
 
 import com.example.ElearningAndExamJLPT.entity.Enrollment;
 import com.example.ElearningAndExamJLPT.repository.IEnrollmentRepository;
+import com.example.ElearningAndExamJLPT.repository.IUserRepository;
 import com.example.ElearningAndExamJLPT.service.IEnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public class EnrollmentServiceImpl implements IEnrollmentService {
     @Autowired
     private IEnrollmentRepository enrollmentRepository;
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     public List<Enrollment> getAll() {
         return null;
@@ -23,6 +30,10 @@ public class EnrollmentServiceImpl implements IEnrollmentService {
 
     @Override
     public Enrollment save(Enrollment entity) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LocalDateTime now = LocalDateTime.now();
+        entity.setUserId(userRepository.findByUsername(authentication.getName()).get().getId());
+        entity.setRegistrationDate(now);
         return enrollmentRepository.save(entity);
     }
 
