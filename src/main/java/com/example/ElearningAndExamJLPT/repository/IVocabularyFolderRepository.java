707a5ee0465d1fe7989437adeafacadf29fe451c
Tan Dung <tandung12072002@gmail.com>
2023-06-06 14:47:13 +0700

    fix

--- src/main/java/com/example/ElearningAndExamJLPT/repository/IVocabularyFolderRepository.java
+++ src/main/java/com/example/ElearningAndExamJLPT/repository/IVocabularyFolderRepository.java
@@ -11,12 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface IVocabularyFolderRepository extends JpaRepository<VocabularyFolder, Long> {
-    @Query("SELECT f FROM VocabularyFolder f WHERE " +
-            "f.title LIKE CONCAT('%',:query, '%')" +
-            "AND (:level IS NULL OR c.level = :level) " +
-            "AND (:chapter IS NULL OR c.title = LIKE CONCAT('%',:chapter, '%') OR (:level IS NULL AND :chapter IS NULL)) " +
-            "AND f.deleted = false")
-    List<VocabularyFolder> searchVocabularyFolders(String query, Level level, String chapter);
+//    @Query("SELECT f FROM VocabularyFolder f WHERE " +
+//            "f.title LIKE CONCAT('%',:query, '%')" +
+//            "AND (:level IS NULL OR c.level = :level) " +
+//            "AND (:chapter IS NULL OR c.title = LIKE CONCAT('%',:chapter, '%') OR (:level IS NULL AND :chapter IS NULL)) " +
+//            "AND f.deleted = false")
+//    List<VocabularyFolder> searchVocabularyFolders(String query, Level level, String chapter);
     List<VocabularyFolder> findAllByDeletedFalse();
     Optional<VocabularyFolder> findVocabularyFolderByDeletedFalseAndId(Long id);
 }
